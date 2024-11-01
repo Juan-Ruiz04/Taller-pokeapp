1d22edac92b518c0c2754c1f3f6c828ae655497a
Juan-Ruiz04 <161732849+Juan-Ruiz04@users.noreply.github.com>
2024-11-01 10:51:05 -0500

    Update home-page.js

--- app/pages/home-page/home-page.js
+++ app/pages/home-page/home-page.js
@@ -19,20 +19,15 @@ class HomePage extends BbvaCoreIntlMixin(CellsPage) {
 
   render() {
 
-    return html`
-    <div style="background: linear-gradient(#D1FEBA,rgb(184, 255, 195));">
-      <cells-template-paper-drawer-panel mode="seamed" id="paperHeaderPanel" disable-swipe="" >
-        <div slot="app_main">
-          
-         <h1 style="color: #2344; font-size: 24px; text-align: center;">${this.t('title')}</h1>
-
-          <poketaller-ui></poketaller-ui>
-        </div>
-      </cells-template-paper-drawer-panel>
-
-    </div> 
-    `;
-  }
+return html`
+  <div style="background: linear-gradient(#D1FEBA, rgb(184, 255, 195));">
+    <cells-template-paper-drawer-panel mode="seamed" id="paperHeaderPanel" disable-swipe>
+      <div slot="app_main">
+        <h1 style="color: #234; font-size: 24px; text-align: center;">${this.t('title')}</h1>
+        <poketaller-ui></poketaller-ui>
+      </div>
+    </cells-template-paper-drawer-panel>
+  </div> 
+`;
 }
-
 window.customElements.define(HomePage.is, HomePage);
