987ea39d818c954622ea6c20e21cb85d9e626653
Juan-Ruiz04 <161732849+Juan-Ruiz04@users.noreply.github.com>
2024-11-01 10:47:02 -0500

    Update home-page-styles.js

--- app/pages/home-page/home-page-styles.js
+++ app/pages/home-page/home-page-styles.js
@@ -1,9 +1,2 @@
 import { css, unsafeCSS } from 'lit-element';
-import * as foundations from '@bbva-web-components/bbva-foundations-styles';
 
-export default css`
-title {
-  color: red;
-  background-color: aqua;
-}
-`;
